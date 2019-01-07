df846a093eb504ebc0a4cd3cee1062754e6ef088
eric <Eric355402@yahoo.com>
2019-01-07 16:27:37 -0700

    seasons app

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+//class component needs constructor with props that will update at component level,super needed to use React.Component original parent constructor, initialize state with this.state{property: null"for use with unknown numbers"}
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    //this is the only time we do direct assignment!!
+    this.state = { lat: null, errorMessage: '' };
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
-serviceWorker.unregister();
+    window.navigator.geolocation.getCurrentPosition(
+      position => {
+        this.setState({ lat: position.coords.latitude });
+      },
+      err => {
+        this.setState({ errorMessage: err.message });
+      }
+    );
+  }
+
+  render() {
+    return (
+      <div>
+        Latitude: {this.state.lat}
+        <br />
+        Error: {this.state.errorMessage}
+      </div>
+    );
+  }
+}
+
+ReactDOM.render(<App />, document.querySelector('#root'));
